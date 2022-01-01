8d5ed650ecb68e0cf588cbab2cc56ff091188fe3
AlanD <alandixon@users.noreply.github.com>
2022-01-01 23:27:35 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DualProperty
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var pi = typeof(MyRelatedDataEntity).GetProperties();
+            Console.WriteLine("{0} properties", pi.Length);
+        }
+
+        private class MyRelatedDataEntity : RelatedDataEntity<long, object>
+        {
+            public override object RelatedEntity { get; set; }
+        }
+
+        private class RelatedDataEntity<TKey, TEntity>
+        {
+            public virtual TEntity RelatedEntity { get; set; }
+        }
+
+    }
+
+}
